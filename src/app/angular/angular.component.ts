185e3a029160ceb89456898b2de91528f6f3da1a
dong <>
2018-04-14 16:52:07 +0900

    할일 가져오기 - 5.html바인딩

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user.service';
+import {TodoVO} from '../domain/todo.vo';
 
 @Component({
   selector: 'app-angular',
@@ -8,10 +9,16 @@ import {UserService} from '../user.service';
 })
 export class AngularComponent implements OnInit {
 
+  todoList: TodoVO[] = [];
+
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.userService.getTodoList().subscribe(body => console.log(body));
+    this.userService.getTodoList()
+      .subscribe(body => {
+        this.todoList = body;
+        console.log(this.todoList);
+      });
   }
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,13 +9,13 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   // todoVO 배열타입으로 리턴 받는다는거임
- /* getTodoList(): Observable<TodoVO[]> {
+  getTodoList(): Observable<TodoVO[]> {
     // api ?-> optional
     return this.http.get<TodoVO[]>('http://www.javabrain.kr:8080/api/todo');
-  }*/
+  }
 
-  getTodoList() {
+  /*getTodoList() {
     // api ?-> optional
     return this.http.get('http://www.javabrain.kr:8080/api/todo');
-  }
+  }*/
 }
