c8f9c1ce416245411c23cb26e3fad897ac6193ec
dong <>
2018-04-14 17:12:46 +0900

    dev / prod 의 서버 환경 설정

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {TodoVO} from './domain/todo.vo';
+import {environment} from '../environments/environment';
 
 @Injectable()
 export class UserService {
 
-  constructor(private http: HttpClient) { }
-
+  private SERVER: string;
+  private headers: HttpHeaders;
+  constructor(private http: HttpClient) {
+    this.SERVER = `${environment.HOST}`;
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json' // json 통신
+    });
+  }
   // todoVO 배열타입으로 리턴 받는다는거임
   getTodoList(): Observable<TodoVO[]> {
     // api ?-> optional
-    return this.http.get<TodoVO[]>('http://www.javabrain.kr:8080/api/todo');
+    return this.http.get<TodoVO[]>(this.SERVER + '/api/todo');
   }
 
   /*getTodoList() {
