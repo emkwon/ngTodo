82b5ef21012e6a2adfb7faa610c131f8ed3ef6b2
dong <>
2018-04-28 11:06:14 +0900

    할일 추가 애니메이션

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,11 +1,27 @@
+///<reference path="../../../node_modules/@angular/animations/src/animation_metadata.d.ts"/>
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user.service';
 import {TodoVO} from '../domain/todo.vo';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-angular',
   templateUrl: './angular.component.html',
-  styleUrls: ['./angular.component.scss']
+  styleUrls: ['./angular.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({opacity: 1, transform: 'translate(0, 0)'})),
+      transition('void => in', [
+        style({opacity: 0, transform: 'translate(-100%, 0)'}),
+        animate(300) // animate(시간, 종료상태)
+      ]),
+      /*transition('* => void', [
+        animate(300, style({transform: 'translate(0, -100%)', opacity: '0'}))
+      ])*/
+    ])
+
+
+  ]
 })
 export class AngularComponent implements OnInit {
 
