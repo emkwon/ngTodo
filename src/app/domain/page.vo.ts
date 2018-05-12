13e0c03920ea116494b2660acefcfe05f040726b
emkwon <dmsal01@naver.com>
2018-05-12 10:35:51 +0900

    페이지객체 선언과 초기화
    필수필드 초기화 방법
    optional연산자 사용방법

---
+++ src/app/domain/page.vo.ts
@@ -0,0 +1,20 @@
+export class PageVO {
+  // pageIndex: number;
+  // pageSize: number;
+  // totalCount: number;
+  // pageOptions: number[];
+
+  /*
+  constructor(pageSize: number) {
+    this.pageSize = pageSize; // java 스타일
+  } */
+
+  // constructor는 하나만..
+  // ? -> optional 연산자, ? 연산자는 반드시 뒤로
+  constructor(public pageIndex: number, public pageSize: number, public totalCount: number,
+              public  pageOption?: number[]) { // 상단에 서언문 없어도 됨, type script 문법
+    if(!pageOption) {
+      pageOption = [5, 15, 30, 60, 120];
+    }
+  }
+}
