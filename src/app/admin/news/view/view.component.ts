317e31a1bd7dfcd548c4e28adfe2b4b5ba2abeac
emkwon <dmsal01@naver.com>
2018-05-12 16:45:58 +0900

    글 삭제

--- src/app/admin/news/view/view.component.ts
+++ src/app/admin/news/view/view.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {AdminService} from '../../admin.service';
 import {NewsVO} from '../../../domain/news.vo';
 import {DomSanitizer} from '@angular/platform-browser';
 import {SafeHtml} from '@angular/platform-browser/src/security/dom_sanitization_service';
-import {MatDialog} from '@angular/material';
+import {MatDialog, MatSnackBar} from '@angular/material';
 import {ViewDialogComponent} from './view.dialog.component';
 
 
@@ -19,6 +19,8 @@ export class ViewComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private adminService: AdminService, private  sanitizer: DomSanitizer
               , private dialog: MatDialog
+              , private router: Router
+              , private snackBar: MatSnackBar
   ) {
     // console.log(this.route.url);
     // view 객체를 호출할때, 첫번째만 생성, 두번째부터는 안 찍힌다.
@@ -50,6 +52,16 @@ export class ViewComponent implements OnInit {
       if (data) {
         // 삭제 로직 구현
         console.log(data);
+        this.adminService.removeNews(news.news_id).subscribe(body => {
+            if (body.result === 0) {
+              this.snackBar.open('삭제되었습니다.', null, {duration: 2000});
+              this.router.navigateByUrl('/admin/news');
+              // 이벤트 뉴스 이벤트 갱신
+              this.adminService.refresh.next(true);
+            }
+
+
+        });
       }
     });
 
