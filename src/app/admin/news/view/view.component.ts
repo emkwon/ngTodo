5dc6d833508c2b3f5ed67fb3c4a333fc1f663513
emkwon <dmsal01@naver.com>
2018-05-12 12:08:42 +0900

    뉴스 상세보기 서비스 구현
    params 데이터타입의 Observable객체에 subscribe해서 변경된 데이터를 가져온다.

--- src/app/admin/news/view/view.component.ts
+++ src/app/admin/news/view/view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AdminService} from '../../admin.service';
 
 @Component({
   selector: 'app-view',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private adminService: AdminService) {
+    // console.log(this.route.url);
+    // view 객체를 호출할때, 첫번째만 생성, 두번째부터는 안 찍힌다.
+    // => 한번만 생성
+    console.log(location.pathname);
+
+    this.route.params.subscribe(parmas => {
+        console.log(parmas);
+        const news_id = +parmas['news_id']; // 앞에 + 붙이면 스트링이 숫자로 변환
+        console.log(news_id);
+        this.findOneNews(news_id);
+    });
+  }
+
+  findOneNews(news_id: number) {
+    this.adminService.findOneNews(news_id).subscribe(body => {
+      console.log(body);
+    });
+  }
 
   ngOnInit() {
   }
